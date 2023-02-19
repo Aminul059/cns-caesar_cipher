117f8c8f10018dc9853e014c64d7bc75831a9c68
Mohammad Aminul Islam <maminul2018@gmail.com>
2023-02-19 14:54:15 +0600

    final commit

--- cns.js
+++ cns.js
@@ -14,3 +14,21 @@ function encryptMessage() {
     }
     document.getElementById("encryptedMessage").innerHTML = encryptedMessage;
   }
+
+
+  function decryptMessage() {
+    var message = document.getElementById("message2").value;
+    var shift = parseInt(document.getElementById("shift2").value);
+    var decryptedMessage = "";
+    for (var i = 0; i < message.length; i++) {
+      var char = message.charCodeAt(i);
+      if (char >= 65 && char <= 90) {
+        decryptedMessage += String.fromCharCode((char - 65 - shift +26) % 26 + 65);
+      } else if (char >= 97 && char <= 122) {
+        decryptedMessage += String.fromCharCode((char - 97 - shift + 26) % 26 + 97);
+      } else {
+        decryptedMessage += message.charAt(i);
+      }
+    }
+    document.getElementById("decryptedMessage").innerHTML = decryptedMessage;
+  }
